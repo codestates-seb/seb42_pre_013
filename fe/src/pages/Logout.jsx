1cb3ed6848652382e2b68b7f14eee8425d4a24e3
Juhyuk-Park <tlsoshy@ajou.ac.kr>
2023-02-16 17:16:40 +0900

    스타일드 컴포넌트 작업중1

--- fe/src/pages/Logout.jsx
+++ fe/src/pages/Logout.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Logout = () => {
     return (
-        <div className="container">
+        <Container>
             <div id="content">
                 <div>
                     <h1>
@@ -13,100 +13,111 @@ const Logout = () => {
                 <form>
                     <ul>
                         <li>
-                            <DomainsList href="https://askubuntu.com">
+                            <Link href="https://askubuntu.com">
                                 <LogoImg 
                                     src='https://cdn.sstatic.net/Sites/askubuntu/Img/apple-touch-icon.png'
                                     alt='ask ubuntu'
                                 />
-                                <Link>askubuntu.com</Link>
-                            </DomainsList>
+                                <Domain>askubuntu.com</Domain>
+                            </Link>
                         </li>
                         <li>
-                            <DomainsList href="https://mathoverflow.net">
+                            <Link href="https://mathoverflow.net">
                                 <LogoImg
                                     src='https://cdn.sstatic.net/Sites/mathoverflow/Img/apple-touch-icon.png?v=8c5ff8612fb4'
                                     alt='math overflow'
                                 />
-                                <Link>mathoverflow.net</Link>
-                            </DomainsList>
+                                <Domain>mathoverflow.net</Domain>
+                            </Link>
                         </li>
                         <li>
-                            <DomainsList href="https://serverfault.com">
+                            <Link href="https://serverfault.com">
                                 <LogoImg
                                     src='https://cdn.sstatic.net/Sites/serverfault/Img/apple-touch-icon.png'
                                     alt='server fault'
                                 />
-                                <Link>serverfault.com</Link>
-                            </DomainsList>
+                                <Domain>serverfault.com</Domain>
+                            </Link>
                         </li>                        
                         <li>
-                            <DomainsList href="https://stackapps.com">
+                            <Link href="https://stackapps.com">
                                 <LogoImg
                                     src='https://cdn.sstatic.net/Sites/stackapps/Img/apple-touch-icon.png'
                                     alt='stack apps'
                                 />
-                                <Link>stackapps.com</Link>
-                            </DomainsList>
+                                <Domain>stackapps.com</Domain>
+                            </Link>
                         </li>                     
                         <li>
-                            <DomainsList href="https://stackexchange.com">
+                            <Link href="https://stackexchange.com">
                                 <LogoImg
                                     src='https://cdn.sstatic.net/Sites/stackexchange/Img/apple-touch-icon@2.png'
                                     alt='stack exchange'
                                 />
-                                <Link>stackexchange.com</Link>
-                            </DomainsList>
+                                <Domain>stackexchange.com</Domain>
+                            </Link>
                         </li>
                         <li>
-                            <DomainsList href="https://stackoverflow.com">
+                            <Link href="https://stackoverflow.com">
                                 <LogoImg
                                     src='https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png'
                                     alt='stack overflow'
                                 />
-                                <Link>stackoverflow.com</Link>
-                            </DomainsList>
-                        </li>                        <li>
-                            <DomainsList href="https://superuser.com">
+                                <Domain>stackoverflow.com</Domain>
+                            </Link>
+                        </li>                        
+                        <li>
+                            <Link href="https://superuser.com">
                                 <LogoImg
                                     src='https://cdn.sstatic.net/Sites/superuser/Img/apple-touch-icon.png'
                                     alt='super user'
                                 />
-                                <Link>superuser.com</Link>
-                            </DomainsList>
+                                <Domain>superuser.com</Domain>
+                            </Link>
                         </li>
                     </ul>
-                    <div>
-                        <div>
+                    <LogoutAll>
+                        <CheckboxDiv>
                             <input type="checkbox" id="everywhere" />
-                        </div>
+                        </CheckboxDiv>
                         <div>
                             <label for="everywhere">Log out on all devices</label>
                         </div>
-                    </div>
+                    </LogoutAll>
                     <div>
                         <button>Log out</button>
-                        <DomainsList href="https://stackoverflow.com/">Cancel</DomainsList>
+                        <Link href="https://stackoverflow.com/">Cancel</Link>
                     </div>
                     <div>
-                        If you’re on DomainsList shared computer, remember to log out of your Open ID provider (Facebook, Google, Stack Exchange, etc.) as well.
+                        If you’re on Link shared computer, remember to log out of your Open ID provider (Facebook, Google, Stack Exchange, etc.) as well.
                     </div>
                 </form>
             </div>
-        </div>
+        </Container>
     )
 }
 
 export default Logout;
 
-const DomainsList = styled.a`
+const Link = styled.a`
     display: flex;
+    align-items: center;
 `
 
 const LogoImg = styled.img`
     width: 16px;
     height: 16px;
+    margin: 4px;
 `
 
-const Link = styled.div`
+const Domain = styled.div`
     margin: 4px;
+`
+
+const LogoutAll = styled.div`
+    display: flex;
+`
+
+const CheckboxDiv = styled.div`
+    margin-right: 4px;
 `
