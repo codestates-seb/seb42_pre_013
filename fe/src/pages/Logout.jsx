17a1184afd686c71ab376357d0e0e003acca0277
Juhyuk-Park <tlsoshy@ajou.ac.kr>
2023-02-17 15:41:25 +0900

    로그아웃 페이지 목업 셋팅 + 네브 수정중

--- fe/src/pages/Logout.jsx
+++ fe/src/pages/Logout.jsx
@@ -1,26 +1,29 @@
 import React from "react"
 import styled from "styled-components";
+import LogoutNav from '../components/nav/LogoutNav'
+
 
 function Logout() {
     return (
-        <Container>
-            <Content>
+        <>
+            <LogoutNav />
+            <Container>
                 <FlexItem>
-                        <H1>
-                            Clicking "Log out" will log you out of the following domains on this device:
-                        </H1>
-                    <form>
-                        <ul>
-                            <li>
+                    <H1>
+                        Clicking "Log out" will log you out of the following domains on this device:
+                    </H1>
+                    <LogoutForm>
+                        <Ul>
+                            <Li>
                                 <Link href="https://askubuntu.com">
-                                    <LogoImg 
+                                    <LogoImg
                                         src='https://cdn.sstatic.net/Sites/askubuntu/Img/apple-touch-icon.png'
                                         alt='ask ubuntu'
                                     />
                                     <Domain>askubuntu.com</Domain>
                                 </Link>
-                            </li>
-                            <li>
+                            </Li>
+                            <Li>
                                 <Link href="https://mathoverflow.net">
                                     <LogoImg
                                         src='https://cdn.sstatic.net/Sites/mathoverflow/Img/apple-touch-icon.png?v=8c5ff8612fb4'
@@ -28,8 +31,8 @@ function Logout() {
                                     />
                                     <Domain>mathoverflow.net</Domain>
                                 </Link>
-                            </li>
-                            <li>
+                            </Li>
+                            <Li>
                                 <Link href="https://serverfault.com">
                                     <LogoImg
                                         src='https://cdn.sstatic.net/Sites/serverfault/Img/apple-touch-icon.png'
@@ -37,8 +40,8 @@ function Logout() {
                                     />
                                     <Domain>serverfault.com</Domain>
                                 </Link>
-                            </li>                        
-                            <li>
+                            </Li>                        
+                            <Li>
                                 <Link href="https://stackapps.com">
                                     <LogoImg
                                         src='https://cdn.sstatic.net/Sites/stackapps/Img/apple-touch-icon.png'
@@ -46,8 +49,8 @@ function Logout() {
                                     />
                                     <Domain>stackapps.com</Domain>
                                 </Link>
-                            </li>                     
-                            <li>
+                            </Li>                     
+                            <Li>
                                 <Link href="https://stackexchange.com">
                                     <LogoImg
                                         src='https://cdn.sstatic.net/Sites/stackexchange/Img/apple-touch-icon@2.png'
@@ -55,8 +58,8 @@ function Logout() {
                                     />
                                     <Domain>stackexchange.com</Domain>
                                 </Link>
-                            </li>
-                            <li>
+                            </Li>
+                            <Li>
                                 <Link href="https://stackoverflow.com">
                                     <LogoImg
                                         src='https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png'
@@ -64,8 +67,8 @@ function Logout() {
                                     />
                                     <Domain>stackoverflow.com</Domain>
                                 </Link>
-                            </li>                        
-                            <li>
+                            </Li>                        
+                            <Li>
                                 <Link href="https://superuser.com">
                                     <LogoImg
                                         src='https://cdn.sstatic.net/Sites/superuser/Img/apple-touch-icon.png'
@@ -73,8 +76,8 @@ function Logout() {
                                     />
                                     <Domain>superuser.com</Domain>
                                 </Link>
-                            </li>
-                        </ul>
+                            </Li>
+                        </Ul>
                         <LogoutAll>
                             <CheckboxDiv>
                                 <input type="checkbox" id="everywhere" />
@@ -83,17 +86,17 @@ function Logout() {
                                 <label for="everywhere">Log out on all devices</label>
                             </div>
                         </LogoutAll>
-                        <div>
-                            <button>Log out</button>
-                            <Link href="https://stackoverflow.com/">Cancel</Link>
-                        </div>
-                        <div>
-                            If you’re on Link shared computer, remember to log out of your Open ID provider (Facebook, Google, Stack Exchange, etc.) as well.
-                        </div>
-                    </form>
+                        <LogoutAndCancel>
+                            <LogoutButton>Log out</LogoutButton>
+                            <Cancel href="https://stackoverflow.com/">Cancel</Cancel>
+                        </LogoutAndCancel>
+                        <Caution>
+                            If you’re on a shared computer, remember to log out of your Open ID provider (Facebook, Google, Stack Exchange, etc.) as well.
+                        </Caution>
+                    </LogoutForm>
                 </FlexItem>
-            </Content>
-        </Container>
+            </Container>
+        </>
     )
 }
 
@@ -101,45 +104,105 @@ export default Logout;
 
 const Container = styled.div`
     width: 100%;
-    height: 100vh;
+    min-height: 100vh;
     background-color: #f1f2f3;
-    `
-const Content = styled.div`
-    width: 100%;
-    background-color: transparent;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     `
 const FlexItem = styled.div`
-    
     `
 
-const H1 = styled.h1`
+const H1 = styled.div`
+    max-width: 445.64px;
     text-align: center;
     margin-bottom: 24px;
+    font-size: 19px;
+    color: #232629;
+`
+
+const LogoutForm = styled.form`
+    box-sizing: border-box;
+    max-width: 316px;
+    min-height: 422.8px;
+    margin: 0 auto;
+    margin-bottom: 24px;
+    padding: 24px;
+    background-color: white;
+    box-shadow: 1px 3px 3px #cccccc;
+    border-radius: 8px;
     `
+
+const Ul = styled.ul`
+    padding-left: 0;
+    margin-bottom: 16px;
+    border-bottom: 1px solid;
+    border-color: #d6d9dc;
+    padding-bottom: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+`
+const Li = styled.li`
+    list-style: none;
+`
 
 const Link = styled.a`
     display: flex;
     align-items: center;
+    text-decoration-line: none;
+    margin: 2px;
 `
 
 const LogoImg = styled.img`
     width: 16px;
     height: 16px;
-    margin: 4px;
+    margin-right: 5px;
 `
 
 const Domain = styled.div`
-    margin: 4px;
+    color: #0074cc;
+    font-size: 14.3px;
 `
 
 const LogoutAll = styled.div`
     display: flex;
+    font-size: 12px;
+    color: #0c0d0e;
+    margin-bottom: 16px;
 `
 
 const CheckboxDiv = styled.div`
     margin-right: 4px;
+`
+const LogoutAndCancel = styled.div`
+    display: flex;
+`
+
+const LogoutButton = styled.button`
+    width: 66.16px;
+    height: 37.78px;
+    color: #ffffff;
+    font-size: 13px;
+    background-color: #0a95ff;
+    margin: 2px;
+    border: none;
+    border-radius: 4px;
+    padding: 10.4px;
+`
+const Cancel = styled.a`
+    height: 37.78px;
+    color: #0074cc;
+    font-size: 13px;
+    margin: 2px;
+    padding: 10.4px;
+    text-decoration-line: none;
+`
+
+const Caution = styled.div`
+    color: #6a737c;
+    font-size: 12px;
+    margin-top: 32px;
 `
