330cbd2436ee0a7a08c8ac66777c990b4ba7fc7e
Min Hyeok <96058996+Min-hh-k@users.noreply.github.com>
2023-02-17 10:33:02 +0900

    로그아웃 페이지

--- fe/src/pages/Logout.jsx
+++ fe/src/pages/Logout.jsx
@@ -1,109 +1,125 @@
 import React from "react"
 import styled from "styled-components";
 
-const Logout = () => {
+function Logout() {
     return (
-        <>
         <Container>
-            <div id="content">
-                <div>
-                    <h1>
-                        Clicking "Log out" will log you out of the following domains on this device:
-                    </h1>
-                </div>
-                <form>
-                    <ul>
-                        <li>
-                            <Link href="https://askubuntu.com">
-                                <LogoImg 
-                                    src='https://cdn.sstatic.net/Sites/askubuntu/Img/apple-touch-icon.png'
-                                    alt='ask ubuntu'
-                                />
-                                <Domain>askubuntu.com</Domain>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="https://mathoverflow.net">
-                                <LogoImg
-                                    src='https://cdn.sstatic.net/Sites/mathoverflow/Img/apple-touch-icon.png?v=8c5ff8612fb4'
-                                    alt='math overflow'
-                                />
-                                <Domain>mathoverflow.net</Domain>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="https://serverfault.com">
-                                <LogoImg
-                                    src='https://cdn.sstatic.net/Sites/serverfault/Img/apple-touch-icon.png'
-                                    alt='server fault'
-                                />
-                                <Domain>serverfault.com</Domain>
-                            </Link>
-                        </li>                        
-                        <li>
-                            <Link href="https://stackapps.com">
-                                <LogoImg
-                                    src='https://cdn.sstatic.net/Sites/stackapps/Img/apple-touch-icon.png'
-                                    alt='stack apps'
-                                />
-                                <Domain>stackapps.com</Domain>
-                            </Link>
-                        </li>                     
-                        <li>
-                            <Link href="https://stackexchange.com">
-                                <LogoImg
-                                    src='https://cdn.sstatic.net/Sites/stackexchange/Img/apple-touch-icon@2.png'
-                                    alt='stack exchange'
-                                />
-                                <Domain>stackexchange.com</Domain>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="https://stackoverflow.com">
-                                <LogoImg
-                                    src='https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png'
-                                    alt='stack overflow'
-                                />
-                                <Domain>stackoverflow.com</Domain>
-                            </Link>
-                        </li>                        
-                        <li>
-                            <Link href="https://superuser.com">
-                                <LogoImg
-                                    src='https://cdn.sstatic.net/Sites/superuser/Img/apple-touch-icon.png'
-                                    alt='super user'
-                                />
-                                <Domain>superuser.com</Domain>
-                            </Link>
-                        </li>
-                    </ul>
-                    <LogoutAll>
-                        <CheckboxDiv>
-                            <input type="checkbox" id="everywhere" />
-                        </CheckboxDiv>
+            <Content>
+                <FlexItem>
+                        <H1>
+                            Clicking "Log out" will log you out of the following domains on this device:
+                        </H1>
+                    <form>
+                        <ul>
+                            <li>
+                                <Link href="https://askubuntu.com">
+                                    <LogoImg 
+                                        src='https://cdn.sstatic.net/Sites/askubuntu/Img/apple-touch-icon.png'
+                                        alt='ask ubuntu'
+                                    />
+                                    <Domain>askubuntu.com</Domain>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="https://mathoverflow.net">
+                                    <LogoImg
+                                        src='https://cdn.sstatic.net/Sites/mathoverflow/Img/apple-touch-icon.png?v=8c5ff8612fb4'
+                                        alt='math overflow'
+                                    />
+                                    <Domain>mathoverflow.net</Domain>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="https://serverfault.com">
+                                    <LogoImg
+                                        src='https://cdn.sstatic.net/Sites/serverfault/Img/apple-touch-icon.png'
+                                        alt='server fault'
+                                    />
+                                    <Domain>serverfault.com</Domain>
+                                </Link>
+                            </li>                        
+                            <li>
+                                <Link href="https://stackapps.com">
+                                    <LogoImg
+                                        src='https://cdn.sstatic.net/Sites/stackapps/Img/apple-touch-icon.png'
+                                        alt='stack apps'
+                                    />
+                                    <Domain>stackapps.com</Domain>
+                                </Link>
+                            </li>                     
+                            <li>
+                                <Link href="https://stackexchange.com">
+                                    <LogoImg
+                                        src='https://cdn.sstatic.net/Sites/stackexchange/Img/apple-touch-icon@2.png'
+                                        alt='stack exchange'
+                                    />
+                                    <Domain>stackexchange.com</Domain>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="https://stackoverflow.com">
+                                    <LogoImg
+                                        src='https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png'
+                                        alt='stack overflow'
+                                    />
+                                    <Domain>stackoverflow.com</Domain>
+                                </Link>
+                            </li>                        
+                            <li>
+                                <Link href="https://superuser.com">
+                                    <LogoImg
+                                        src='https://cdn.sstatic.net/Sites/superuser/Img/apple-touch-icon.png'
+                                        alt='super user'
+                                    />
+                                    <Domain>superuser.com</Domain>
+                                </Link>
+                            </li>
+                        </ul>
+                        <LogoutAll>
+                            <CheckboxDiv>
+                                <input type="checkbox" id="everywhere" />
+                            </CheckboxDiv>
+                            <div>
+                                <label for="everywhere">Log out on all devices</label>
+                            </div>
+                        </LogoutAll>
                         <div>
-                            <label for="everywhere">Log out on all devices</label>
+                            <button>Log out</button>
+                            <Link href="https://stackoverflow.com/">Cancel</Link>
                         </div>
-                    </LogoutAll>
-                    <div>
-                        <button>Log out</button>
-                        <Link href="https://stackoverflow.com/">Cancel</Link>
-                    </div>
-                    <div>
-                        If you’re on Link shared computer, remember to log out of your Open ID provider (Facebook, Google, Stack Exchange, etc.) as well.
-                    </div>
-                </form>
-            </div>
+                        <div>
+                            If you’re on Link shared computer, remember to log out of your Open ID provider (Facebook, Google, Stack Exchange, etc.) as well.
+                        </div>
+                    </form>
+                </FlexItem>
+            </Content>
         </Container>
-        </>
     )
 }
 
 export default Logout;
 
 const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #f1f2f3;
+    `
+const Content = styled.div`
+    width: 100%;
+    background-color: transparent;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    `
+const FlexItem = styled.div`
+    
+    `
 
-`
+const H1 = styled.h1`
+    text-align: center;
+    margin-bottom: 24px;
+    `
 
 const Link = styled.a`
     display: flex;
