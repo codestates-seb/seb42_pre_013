cc04f2ed3a0907e24ca7c5af5cded888225ea02d
Juhyuk-Park <tlsoshy@ajou.ac.kr>
2023-02-27 16:28:31 +0900

    create : login 기능 구현중(쿠키 활용)

--- fe/src/components/login/LoginForm.jsx
+++ fe/src/components/login/LoginForm.jsx
@@ -1,16 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import axios from "axios";
+import { Cookies } from "react-cookie";
+
+// 쿠키 함수(토큰을 저장하기 위함)
+const cookies = new Cookies();
+// 쿠키에 값을 저장할 때
+// option에는 쿠키의 만료기한이 들어간다. -1을 해주면 쿠키가 지워진다.
+const setCookie = (name, value, option) => {
+  return cookies.set(name, value, { ...option });
+};
+// 쿠키에 있는 값을 꺼낼 때
+const getCookie = (name) => {
+  return cookies.get(name);
+};
+// 쿠키를 지울 때
+const removeVookie = (name) => {
+  return cookies.remove(name);
+};
+
+// 서버 URL을 할당하는 변수는
+const SERVER_URL = "http://localhost:4000/api/users/login";
 
 function LoginForm() {
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const response = await axios.post(SERVER_URL, { email, password });
+
+    // response에서 토큰을 꺼낸다.
+    const accessToken = response.data.token;
+
+    //  setCookies 함수의 첫번째 인자는 쿠키 이름, 두번째 인자는 값.
+    setCookie("is_login", `${accessToken}`);
+    console.log(response);
+    return response.data;
+  };
+
   return (
     <>
       <LoginFm>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <label>Email</label>
-          <input type="email" required />
+          <input type="email" name="email" required />
           <label>Password</label>
-          <input type="password" required />
-          <LoginBtn>Log in</LoginBtn>
+          <input type="password" name="password" required />
+          <LoginBtn type="submit" value="Log in" />
         </form>
       </LoginFm>
     </>
@@ -45,7 +81,7 @@ const LoginFm = styled.div`
   }
 `;
 
-const LoginBtn = styled.button`
+const LoginBtn = styled.input`
   width: 250px;
   height: 41px;
   font-size: 13px;
